8cafadb9aefd9fd6503ec33329662f2efc6c1e2d
gussj945 <gussj945@student.liu.se>
2025-10-28 09:08:04 +0100

    lab1 b done?

--- lab1/task1/client.py
+++ lab1/task1/client.py
@@ -2,13 +2,15 @@ import logging
 import websockets
 import asyncio
 from websockets.asyncio.client import connect
+import sys
 
 
 
 async def connectToServer():
-    connection = await connect("ws://localhost:10000/") #connect to server (PORT 10000)
+    server = sys.argv[1]
+    connection = await connect(f"ws://localhost:{server}/") #connect to server (PORT 10000)
     # Connect to server
-    await connection.send("Ping") # Send a message
+    await connection.send("ping") # Send a message
     response = await connection.recv() # Receive next message
     await connection.close() # Close
 
--- lab1/task1/server.py
+++ lab1/task1/server.py
@@ -2,6 +2,7 @@ import logging
 import websockets
 import asyncio
 from websockets.asyncio.server import serve
+import sys
 
 
 logging.basicConfig(
@@ -9,14 +10,39 @@ format= "%(asctime)s %(message)s",
 level=logging.DEBUG,
 )
 
+async def forward_ping(other_port):
+    try:
+        uri = f"ws://localhost:{other_port}"
+        async with websockets.connect(uri) as websocket:
+            await websocket.send("ping")
+            response = await websocket.recv()
+            return response
+    except Exception as e: 
+        logging.exception(f"error in forward_ping: {e}")
 
-async def handler(websocket):
-    async for message in websocket:
-        await websocket.send("pong")
+
+
+async def handler(websocket, other_port=None):
+    try:
+        async for message in websocket:
+            if message == "ping":
+                if other_port:
+                    pong_msg = await forward_ping(other_port)
+                    await websocket.send(pong_msg)
+            else:
+                await websocket.send("pong")
+    except Exception as e: 
+        logging.exception(f"error in handler: {e}")
 
 
 async def main():
-    async with serve(handler,"",10000) as server:
+    own_port = int(sys.argv[1]) if len(sys.argv) > 1 else 10000
+    second_port = int(sys.argv[2]) if len(sys.argv) > 2 else None
+    async with serve(
+        lambda ws: handler(ws, second_port),
+        "localhost",
+        own_port
+    ) as server:
         await server.serve_forever()
 
 if __name__ == "__main__":
