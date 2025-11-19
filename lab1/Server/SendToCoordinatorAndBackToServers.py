25b77564d54a798c396a6d49870aef10c2b15f49
gussj945 <gussj945@student.liu.se>
2025-11-19 14:47:51 +0100

    all functions use gather

--- lab1/Server/SendToCoordinatorAndBackToServers.py
+++ lab1/Server/SendToCoordinatorAndBackToServers.py
@@ -28,17 +28,20 @@ class storage:
                 case "modify":
                     message = request.get("Message", "")
                     index = request.get("Index", "")
+                    await self.localStorage.modify(index, message, senderID)
                     for i, proxy in enumerate(self.serversToInform):
                         if i != senderID:    
                             tasks.append(proxy.modify(index, message, self.myID))
                    
                 case "delete":
                     index = request.get("Index", "")
+                    await self.localStorage.delete(index, senderID)
                     for i, proxy in enumerate(self.serversToInform):
                         if i != senderID:
                             tasks.append(proxy.delete(index, self.myID))
                   
                 case "deleteall":
+                    await self.localStorage.deleteAll(senderID)
                     for i, proxy in enumerate(self.serversToInform):
                         if i != senderID:    
                             tasks.append(proxy.deleteAll(self.myID))
@@ -62,11 +65,11 @@ class storage:
         if self.myID == self.coordinatorID and senderID != self.coordinatorID:
             #forward to all servers
             request = {"Operation": "put", "Message": message}
-            return await self.notify_proxies(request, self.myID) #senderID
+            return await self.notify_proxies(request, self.myID)
         elif senderID == -1: #change place on this and the next if statement
             #forward to coordinator
             return await self.serversToInform[self.coordinatorID].put(message, senderID)
-        elif senderID == self.coordinatorID:# and self.myID != self.coordinatorID:
+        elif senderID == self.coordinatorID:
             #call comes from coordinator execute locally
             return await self.localStorage.put(message, senderID) 
 
@@ -81,41 +84,39 @@ class storage:
         return result
     
     async def modify(self, index, message, senderID=0): 
-        if senderID == -1:
-            #forward to coordinator
-            return await self.serversToInform[self.coordinatorID].modify(index, message, self.myID)            
-        elif self.myID == self.coordinatorID and senderID != self.coordinatorID:
+        if self.myID == self.coordinatorID and senderID != self.coordinatorID:
             #forward to all servers
             request = {"Operation": "modify", "Index": index, "Message": message}
-            await self.localStorage.modify(index, message, senderID)
             return await self.notify_proxies(request, self.myID)
+        elif senderID == -1: #change place on this and the next if statement
+            #forward to coordinator
+            return await self.serversToInform[self.coordinatorID].modify(index, message, senderID)
         elif senderID == self.coordinatorID:
             #call comes from coordinator execute locally
-            return await self.localStorage.modify(index, message, senderID)
+            return await self.localStorage.modify(index, message, senderID) 
 
     async def delete(self, index, senderID=0): 
-        if senderID == -1:
-            # this message comes from a client, forward to coordinator
-            return await self.serversToInform[self.coordinatorID].delete(index, self.myID)
-            # forward to other servers
-        elif self.myID == self.coordinatorID and senderID != self.coordinatorID:
+        if self.myID == self.coordinatorID and senderID != self.coordinatorID:
+            #forward to all servers
             request = {"Operation": "delete", "Index": index}
-            await self.localStorage.delete(index, senderID)
             return await self.notify_proxies(request, self.myID)
+        elif senderID == -1: #change place on this and the next if statement
+            #forward to coordinator
+            return await self.serversToInform[self.coordinatorID].delete(index, senderID)
         elif senderID == self.coordinatorID:
+            #call comes from coordinator execute locally
             return await self.localStorage.delete(index, senderID)
-
+        
     async def deleteAll(self, senderID=0): 
-        if senderID == -1:
-            # this message comes from a client
-            # forward to coordinator
-            return await self.serversToInform[self.coordinatorID].deleteAll(self.myID)
-            # forward to other servers
-        elif self.myID == self.coordinatorID and senderID != self.coordinatorID:
+        if self.myID == self.coordinatorID and senderID != self.coordinatorID:
+            #forward to all servers
             request = {"Operation": "deleteAll"}
-            await self.localStorage.deleteAll(senderID)
             return await self.notify_proxies(request, self.myID)
+        elif senderID == -1: #change place on this and the next if statement
+            #forward to coordinator
+            return await self.serversToInform[self.coordinatorID].deleteAll(senderID)
         elif senderID == self.coordinatorID:
+            #call comes from coordinator execute locally
             return await self.localStorage.deleteAll(senderID)
 
     async def close(self, senderID=0): 
